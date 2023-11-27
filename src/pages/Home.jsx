88ca32c6bf2584f7be81bbc493c122eced4f8cdb
GODCarNageOP <yashvharale@gmail.com>
2023-11-27 19:06:45 +0530

    speak functionality added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "../assets/cloud.png";
 import "regenerator-runtime/runtime";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import { commands } from "../data/Commands";
+import { useSpeechSynthesis } from 'react-speech-kit';
 
 const Home = () => {
- 
+
+  const { speak } = useSpeechSynthesis();
+
+  const Greet =()=> {
+    var day = new Date();
+    var hr = day.getHours();
+
+    if(hr >= 0 && hr < 12) {
+        speak({text: "Good Morning Boss"});
+    }
+
+    else if(hr == 12) {
+        speak({text:"Good noon Boss"});
+    }
+
+    else if(hr > 12 && hr <= 17) {
+        speak({text:"Good Afternoon Boss"});
+    }
+
+    else {
+        speak({text:"Good Evening Boss"});
+    }
+}
+
+useEffect(()=> {
+  speak({text: "Activating Shadow"}),
+  speak({text: "Shadow online"})
+  Greet()
+},[])
+
   const { browserSupportsSpeechRecognition } = useSpeechRecognition({
     commands
   });
@@ -18,11 +48,11 @@ const Home = () => {
 
   return (
     <div className="bg-transparent h-screen w-full">
-      <div className="container flex items-center justify-center m-auto p-20">
+      <div className="container flex items-center justify-center m-auto pt-20" onClick={()=>greet()}>
         <img
           src={logo}
           alt=""
-          className="h-96 w-96"
+          className="h-80 w-80"
           onClick={SpeechRecognition.startListening}
         />
       </div>
